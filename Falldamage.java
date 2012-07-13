0006a3279188eb6c6826821f1c672bc51cf228f5
Dakota <dstrickner@gmail.com>
2012-07-13 12:08:31 -0400

    worked on main class and listener. still needs a lot to get to even a rough semi working version.

---
+++ Falldamage.java
@@ -0,0 +1,23 @@
+package me.comp.Falldamge;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Falldamage extends JavaPlugin {
+	public static Falldamage plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+	   public void onDisable() {
+			  PluginDescriptionFile pdffile = this.getDescription();
+			  this.logger.info(pdffile.getName() + "is now disabled.");
+		   }
+	   
+	   public void onEnable() {
+			  PluginDescriptionFile pdffile = this.getDescription();
+			  this.logger.info(pdffile.getName() + "is now enabled.");
+			  getConfig().options().copyDefaults(true);
+			  this.saveDefaultConfig();
+		   }
+}
---
+++ Falldamgelistener.java
@@ -0,0 +1,13 @@
+package me.comp.Falldamge;
+
+import java.util.logging.Logger;
+
+import org.bukkit.event.Listener;
+
+public class Falldamgelistener implements Listener {
+
+	private final Falldamage plugin;
+	public Falldamagelistener(Falldamage instance) {
+		plugin = instance;
+	}
+}
